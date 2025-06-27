341db1285416cdb48765ca6f2bd9b5f2b38c0549
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-06-27 15:01:52 +0530

    trpc server component setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-"use client"
+import {  getQueryClient, trpc } from "@/trpc/server"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-import { Button } from "@/components/ui/button";
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-const Page = () => {
-  const trpc = useTRPC();
-  const {data} = useQuery(trpc.createAI.queryOptions({
-    text: "Antonio"
-  }))
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text : "HEllo Prefetching"}));
 
   return (
-    <div className="">
-       {JSON.stringify(data)}
-    </div>
+     <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="">
+         <Suspense fallback={<p>Loading....</p>}>
+            <Client/>
+         </Suspense>
+       </div>
+     </HydrationBoundary>
   )
 }
 
